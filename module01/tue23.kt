e43685d286367f322b7badbedb8298268b19726e
Zerulith <157506877+Zerulith@users.noreply.github.com>
2024-01-23 11:38:41 -0600

    create module 1

---
+++ module01/tue23.kt
@@ -0,0 +1,13 @@
+fun main() {
+    println("The AshenOne raises up from the stranded graveyard")
+    
+    val AshenOneName: String = "Madrigal"
+    println(AshenOneName)
+    
+    var playerlevel: Int = 4
+    println(playerlevel)
+
+    println("The AshenOne embarks on his journey to pick up the Firekeeper in a dungeon")
+    playerlevel += 1
+    println(playerlevel)
+}
