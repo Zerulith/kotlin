f0c9e71a839387cd478b8e38254d0e3a2fe2b7cc
Zerulith <157506877+Zerulith@users.noreply.github.com>
2024-01-25 11:12:12 -0600

    save old version

---
+++ module01/thu25.kt
@@ -0,0 +1,14 @@
+const val HUNTER_NAME = "Madrigal"
+
+fun main() {
+    println("The Hunter awakens in Iosefka's clinic, the ominous strong smell of blood thick in the air")
+
+    println(HUNTER_NAME)
+
+    var playerlevel = 4
+    println(playerlevel)
+
+    println("The Hunter embarks on his journey to uncover the truth about the plague of beasts")
+    playerlevel += 1
+    println(playerlevel)
+}
