a4b4e1605370f7fd73a03b05504b6ca2435c870c
Zerulith <157506877+Zerulith@users.noreply.github.com>
2024-02-13 11:05:18 -0600

    Update salary.kt

--- _review/unit02/salary.kt
+++ _review/unit02/salary.kt
@@ -1,1 +1,12 @@
-
+fun main (args: Array<String>) {
+    println("Hello World!")
+    var name: String = "John Doe"
+    var age: Int = 0
+    var annualPay: Double = 100000000.00
+    
+    println("My name is " + name + ", my age is " + age + ".")
+    
+//    my name is John Doe, my age is 26 and 
+//    I hope to earn 100000000.00 per year. 
+            
+}
