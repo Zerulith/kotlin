7023764616bee42a640cec733fa19f3db7b800c1
Zerulith <157506877+Zerulith@users.noreply.github.com>
2024-02-15 11:08:06 -0600

    Romans

---
+++ _review/unit02/Romans.kt
@@ -0,0 +1,24 @@
+fun main (args: Array<String>) {
+print("Enter a number between 1 and 10")
+    
+    var number = readLine()!!
+    
+    var rom = number.toInt()
+    
+    if (rom ==1) {
+        println("The roman numeral is 1.")
+    }
+    
+//    If the number is less than 1 or higher than 10 the print ERROR
+//    If the number is 1 then print I
+//    If the number is 1 then print II
+//    If the number is 1 then print IIII
+//    If the number is 1 then print IV
+//    If the number is 1 then print V
+//    If the number is 1 then print VI
+//    If the number is 1 then print VII
+//    If the number is 1 then print VIII
+//    If the number is 1 then print IX
+//    If the number is 1 then print X
+    
+}
