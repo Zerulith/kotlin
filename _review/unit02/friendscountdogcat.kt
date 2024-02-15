1796948f339c6b26978ded0f811fa051133a4793
Zerulith <157506877+Zerulith@users.noreply.github.com>
2024-02-15 10:44:57 -0600

    friendscount

---
+++ _review/unit02/friendscountdogcat.kt
@@ -0,0 +1,8 @@
+fun main (args: Array<String>) {
+    var friendsCount = 200
+    if (friendsCount < 500) {
+        print ("Adopt a Dog Today!")
+    } else {
+        println("Buy Cat Food At 20% Off")
+    }
+}
