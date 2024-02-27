86c7935a7a31beaa01156195406dca86b1db7456
Zerulith <157506877+Zerulith@users.noreply.github.com>
2024-02-27 10:37:02 -0600

    Create SumNumbers.kt

---
+++ unit07/SumNumbers.kt
@@ -0,0 +1,27 @@
+fun main() {
+
+    var i = 1
+    while (i <= 50) {
+        println(i)
+        i++
+    }
+
+    println()
+
+    for (i in 1..50) {
+        println(i)
+    }
+
+    println()
+
+    i = 0
+    while (i <= 50) {
+        println("Numbers")
+        i++
+    }
+
+    i=0
+    println()
+
+
+}
